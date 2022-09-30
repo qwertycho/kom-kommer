3759707160d1f5aadf0bb0d496f3211de07f91b3
qwertycho <tychovopstal@gmail.com>
2022-09-30 22:10:51 +0200

    auth functie en api security

--- public/script/feitjes.js
+++ public/script/feitjes.js
@@ -1,25 +1,3 @@
-// xml get request
-// var xmlhttp = new XMLHttpRequest();
-// xmlhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//         var myObj = JSON.parse(this.responseText);
-//         console.log(myObj.feitjes);
-//         setFeitjes(myObj);
-//     }
-// };
-// xmlhttp.open("GET", "/data", true);
-// xmlhttp.send();
-
-// function setFeitjes(myObj) {
-//     var feitjes = myObj.feitjes;
-//     const list = document.createElement('ol');
-//     for (let i = 0; i < feitjes.length; i++) {
-//         const listItem = document.createElement('li');
-//         listItem.innerHTML = feitjes[i];
-//         list.appendChild(listItem);
-//     }
-//     document.getElementById('feitjes').appendChild(list);
-// }
 async function request(url, method, data){
     return new Promise((resolve, reject) => {
     let xmlhttp = new XMLHttpRequest();
@@ -40,7 +18,6 @@ async function request(url, method, data){
     }
     });
 }
-
 
 async function showFeitjes(){
     let feitjes = await request('/dashboard/feitjes', 'GET');
--- routes/admin.js
+++ routes/admin.js
@@ -3,28 +3,28 @@ const express = require('express');
 const router = express.Router();
 const cookieParser = require('cookie-parser');
 router.use(cookieParser())
+const { json } = require('express/lib/response.js');
+
+// eigen troep
+const login = require('../server/login.js');
 const cookie = require('../server/disclaimer.js');
 const data = require('../public/data/data.json');
 const navigatie = require('../server/nav.js');
 const nav = navigatie.navBuilder(data);
 const database = require('../server/database.ts');
-const { json } = require('express/lib/response.js');
 
 router.post('/login', (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-    if(username == process.env.admin && password == process.env.password){
+    if(login.admin(req.body.auth, req.body.username, req.body.password)){
         res.cookie('auth', process.env.auth, { maxAge: 900000, httpOnly: true });
         res.redirect('/dashboard');
     } else {
-        console.log("wrong credentials");
+        console.log("login: verkeerde gegevens");
         // loggen naar database
     }
 });
 
 router.get('/feitjes', (req, res) => {
     console.log("request for feitjes");
-    console.log(req.body);
     try{
         database.loadFeitjes().then((result) => {
             res.send(JSON.stringify(result));
@@ -35,53 +35,62 @@ router.get('/feitjes', (req, res) => {
     }
 }).post('/feitjes', (req, res) => {
     console.log("request for post feitjes");
-    console.log(req.body);
-    try{
-        database.addFeitje(req.body.feit).then(() => {
-            res.send(JSON.stringify(req.body.feit));
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).send();
+    if(login.admin(req.cookies.auth)){
+        try{
+            database.addFeitje(req.body.feit).then(() => {
+                res.send(JSON.stringify(req.body.feit));
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).send();
+        }
+    } else {
+        console.log("post request feitjes: verkeerde auth");
+        res.status(401).send();
     }
 })
 router.delete('/feitjes', (req, res) => {
     console.log("delete request");
-    console.log("request for  put in feitjes");
-    console.log(req.body);
-    try{
-        database.deleteFeitje(req.body.id).then(() => {
-            res.send(JSON.stringify(req.body.id));
-        });
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
+    if(login.admin(req.cookies.auth)){
+        console.log("request for  put in feitjes");
+        try{
+            database.deleteFeitje(req.body.id).then(() => {
+                res.send(JSON.stringify(req.body.id));
+            });
+        }
+        catch (err) {
+            console.log(err);
+            res.status(500).send();
+        }
+    } else {
+        console.log("delete request feitjes: verkeerde auth");
+        res.status(401).send();
     }
 })
 
 router.put('/feitjes', (req, res) => {
     console.log("request for  put in feitjes");
-    console.log(req.body);
-    try{
-        database.updateFeitje(req.body.id, req.body.feit).then(() => {
-            res.send(JSON.stringify(req.body.id));
-        });
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send();
+        if(login.admin(req.cookies.auth)){
+        try{
+            database.updateFeitje(req.body.id, req.body.feit).then(() => {
+                res.send(JSON.stringify(req.body.id));
+            });
+        }
+        catch (err) {
+            console.log(err);
+            res.status(500).send();
+        }
+    } else {
+        console.log("put request feitjes: verkeerde auth");
+        res.status(401).send();
     }
 });
 
-
 router.get('/', (req, res) => {
-    console.log(req.cookies);
-    if(req.cookies.auth == process.env.auth){
-        console.log("logged in");
+    if(login.admin(req.cookies.auth)){
         res.render('../views/dashboard', {footer:  data.footer.text, nav: nav, disclaimer: cookie.checkCookies(req.cookies)});
     } else {
-        console.log("not logged in");
+        console.log("dashboard: verkeerde auth");
         res.render('../views/login', {footer:  data.footer.text, nav: nav, disclaimer: cookie.checkCookies(req.cookies)});
     }
 });
